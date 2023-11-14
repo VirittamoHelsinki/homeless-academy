293984c5d780fab24f7b455d8bec834e475f6a8d
Henna <57915266+hnnav@users.noreply.github.com>
2023-11-14 12:37:50 +0200

    Add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <img src='/src/assets/logo.png'/>
+      <p>HOMELESS ACADEMY</p>
+      <p>DESIRE TO CHANGE</p>
+    </div>
+  )
+}
+
+export default Navbar;

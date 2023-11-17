be454401eab31b9ce943ec2546b40c69aa08f7f2
Mirazum <mira.sust@gmail.com>
2023-11-17 08:56:35 +0200

    solving the conflict

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <main className='app__wrapper'>
       <Navbar language={language} setLanguage={setLanguage} />
-      <Footer />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />

065d9d41fb709708d3ada05b8b76082e4e84c4f5
Mirazum <mira.sust@gmail.com>
2023-11-20 13:21:05 +0200

    Adding the our values section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Activities from '../components/Activities/Activities';
+import OurValues from '../components/OurValues/OurValues';
 import Testimonials from '../components/Testimonials/Testimonials';
 
 function About() {
@@ -7,6 +8,8 @@ function About() {
     <div>
       <Activities></Activities>
       <Testimonials></Testimonials>
+      <OurValues></OurValues>
+
     </div>
   )
 }

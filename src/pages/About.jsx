741ebab8834b5f9cfd136596c495971cc16666e2
Mirazum <mira.sust@gmail.com>
2023-11-21 09:05:27 +0200

    removing env file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Activities from '../components/Activities/Activities';
 import OurValues from '../components/OurValues/OurValues';
 import Testimonials from '../components/Testimonials/Testimonials';
+import CoWorkers from '../components/CoWorkers/CoWorkers';
 
 function About() {
   return (
@@ -9,6 +10,7 @@ function About() {
       <Activities></Activities>
       <Testimonials></Testimonials>
       <OurValues></OurValues>
+      <CoWorkers></CoWorkers>
 
     </div>
   )

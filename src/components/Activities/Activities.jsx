fcc34d77b51253c1501416ae0e7523b79b3d3351
Henna <57915266+hnnav@users.noreply.github.com>
2023-11-23 15:24:43 +0200

    Fix activities section responsivity

--- src/components/Activities/Activities.jsx
+++ src/components/Activities/Activities.jsx
@@ -26,36 +26,40 @@ const Activities = () => {
   return (
     <div>
       {activities && 
-        <div className='lg:grid lg:grid-cols-2 bg-dark-green laptop:gap-y-5'>
-          <div className='lg:ml-16 lg:pt-16'>
-            <h1 className='text-white  lg:pb-5 font-sans text-xl lg:text-2xl font-bold pt-10 text-center lg:text-left'>
-              {activities.title1}
-            </h1>
-            <p className='text-white text-center font-sans text-base lg:text-lg  p-10 pt-6 lg:p-2 w-full laptop:w-full lg:text-left'>
-              {activities.description1}
-            </p>
+        <div className='pt-16 lg:pt-0 bg-dark-green flex flex-col gap-8 lg:gap-16'>
+
+          <div className='flex flex-col lg:flex-row gap-6'>
+            {/* Text 1 */}
+            <div className='flex flex-col gap-6 px-10 flex-1 lg:pt-14'>
+              <h1 className='text-white font-sans text-xl xl:text-3xl font-bold text-center'>
+                {activities.title1}
+              </h1>
+              <p className='text-white text-center font-sans text-base xl:text-xl'>
+                {activities.description1}
+              </p>
+            </div>
+
+            {/* Image 1 */}
+            <div className='flex-1'>
+              <img className='w-full lg:h-full lg:object-cover' src={activities.image1.fields.file.url} alt='Football' />
+            </div>
           </div>
-          <div className=' lg:p-4 lg:pt-0 lg:pr-0 lg:ml-4 '>
-            <img className='w-full' src={activities.image1.fields.file.url} alt='Football' />
-          </div>
-          <div className=' lg:hidden'>
-            <h1 className='text-white  lg:pb-5 font-sans text-xl lg:text-2xl font-bold  lg:p-2 pt-10 text-center lg:text-left'>
-              {activities.title2}
-            </h1>
-            <p className='text-white text-center font-sans text-base lg:text-lg  p-10 pt-6 lg:p-2 w-full  lg:w-11/12 lg:text-left'>
-              {activities.description2}
-            </p>
-          </div>
-          <div className='lg:p-4 lg:pb-0 lg:pl-0 lg:mr-4 laptop:pt-18 '>
-            <img className='w-full' src={activities.image2.fields.file.url} alt='Football' />
-          </div>
-          <div className='hidden  lg:flex flex-col lg:ml-14 lg:mr-12 lg:pt-12'>
-            <h1 className='text-white pb-5 font-sans text-2xl font-bold'>
-              {activities.title2}
-            </h1>
-            <p className='text-white font-sans text-lg  w-5/6 lg:w-11/12 laptop:pb-4'>
-              {activities.description2}
-            </p>
+
+          <div className='flex flex-col lg:flex-row-reverse gap-6'>
+            {/* Text 2 */}
+            <div className='flex flex-col gap-6 px-10 flex-1 lg:pb-10'>
+              <h1 className='text-white font-sans text-xl xl:text-3xl font-bold text-center'>
+                {activities.title2}
+              </h1>
+              <p className='text-white text-center font-sans text-base xl:text-xl'>
+                {activities.description2}
+              </p>
+            </div>
+
+            {/* Image 2 */}
+            <div className='flex-1'>
+              <img className='w-full lg:h-full lg:object-cover' src={activities.image2.fields.file.url} alt='Football' />
+            </div>
           </div>
         </div>
       }

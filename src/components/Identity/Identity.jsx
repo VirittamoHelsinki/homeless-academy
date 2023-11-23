59a80638438530208f6fd103dfeddfb5a59f5a99
Henna <57915266+hnnav@users.noreply.github.com>
2023-11-23 12:06:32 +0200

    Translate homepage

--- src/components/Identity/Identity.jsx
+++ src/components/Identity/Identity.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import AppContext from '../../AppContext';
-import img from '../../assets/unsplash.png'
 
 const Identity = ({ introduction, image2, subtitle, activitiesDescription1, activitiesDescription2}) => {
   const { language } = useContext(AppContext);
@@ -8,17 +7,29 @@ const Identity = ({ introduction, image2, subtitle, activitiesDescription1, acti
   return (
     <div>
       <div className='bg-light-green'>
-        <h1 className='text-center text-2xl font-bold lg:text-6xl pb-2 pt-8 lg:pb-4 lg:pt-12 font-sans '><span className='text-dark-green'>{language === 'fi-FI' ? 'Keitä' : 'Who'}</span> {language === 'fi-FI' ? 'olemme?' : 'are we?'}</h1>
-        <p className='text-center lg:text-xl text-light-gray font-semibold w-11/12 p-8 lg:w-2/5 mx-auto pb-12 '>Homeless Academy ry on voittoa tavoittelematon yhdistys, joka tuottaa ja kehittää asunnottomien ja päihdekuntoutujien toiminnallisen kuntoutuksen menetelmiä. Yhtenä toimintamme muotona järjestämme, kehitämme ja koordinoimme jalkapallotoimintaa Suomessa, erityisesti katujalkapallotoimintaa. Toimintaa järjestetään sekä naisille että miehille.</p>
+        <h1 
+          className='text-center text-2xl font-bold lg:text-6xl pb-2 pt-8 lg:pb-4 lg:pt-12 font-sans'>
+          <span className='text-dark-green'>{language === 'fi-FI' ? 'Keitä' : 'Who'}</span> {language === 'fi-FI' ? 'olemme?' : 'are we?'}
+        </h1>
+        <p 
+          className='text-center lg:text-xl text-light-gray font-semibold w-11/12 p-8 lg:w-2/5 mx-auto pb-12'>
+          {introduction && introduction}
+        </p>
       </div>
       <div className='bg-light-green flex items-center justify-between mx-auto mb-[-50px] lg:mb-[-190px]'>
-        <img className='mx-auto w-4/6 h-4/6 lg:w-2/5 lg:h-2/5' src={img} alt="image" />
+        {image2 && <img className='mx-auto w-4/6 h-4/6 lg:w-2/5 lg:h-2/5' src={image2} alt='image' />}
       </div>
-      <div className='bg-dark-green '>
+      <div className='bg-dark-green'>
         <div>
-          <h2 className='text-center text-white  text-xl lg:text-4xl font-lexend  pt-20 lg:pt-60  font-semibold'>Toimintaamme voidaan kutsua<br></br> sosiaaliseksi innovaatioksi</h2>
-          <p className='text-center text-white w-3/4 lg:w-1/3 mx-auto pb-10 pt-10 lg:text-xl font-lexend'>Toiminnalla on suuria positiivisia vaikutuksia yksilöiden elämään, jotka toiminnassa ovat mukana. Yksilön hyvinvoinnin kautta positiiviset vaikutukset ulottuvat yhteiskunnan tasolle.  </p>
-          <p className='text-center text-white w-3/4 lg:w-1/3 mx-auto pb-14 lg:text-xl font-lexend'>Tämä toteutuu sillä, että kohderyhmämme henkilöille tuotetaan toimintaa, jonka kautta he voivat saada positiivisia elämyksiä omassa vertaisryhmässään sekä itsetunnon ja päihteettömän identiteetin kasvun myötä tavoitella tuetusti mm. opiskelu- ja työpaikkaa sekä omaa pysyvää asuntoa.  </p>
+          <h2 className='text-center text-white px-[10%] md:px-[20%] text-xl lg:text-4xl font-lexend pt-20 lg:pt-60 font-semibold'>
+            {subtitle && subtitle}
+          </h2>
+          <p className='text-center text-white w-3/4 lg:w-1/3 mx-auto pb-10 pt-10 lg:text-xl font-lexend'>
+            {activitiesDescription1 && activitiesDescription1}
+          </p>
+          <p className='text-center text-white w-3/4 lg:w-1/3 mx-auto pb-14 lg:text-xl font-lexend'>
+            {activitiesDescription2 && activitiesDescription2}
+          </p>
         </div>
       </div>
     </div>

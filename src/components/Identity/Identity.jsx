a5a63bd377912f61fb58a93491b145731af6c56e
Henna <57915266+hnnav@users.noreply.github.com>
2023-11-24 09:02:43 +0200

    Fix identity section css

--- src/components/Identity/Identity.jsx
+++ src/components/Identity/Identity.jsx
@@ -9,18 +9,18 @@ const Identity = ({ introduction, image2, subtitle, activitiesDescription1, acti
     <div>
       <div className='bg-light-green'>
         <h1 
-          className='text-center text-2xl font-bold lg:text-6xl pb-2 pt-8 lg:pb-4 lg:pt-12 font-sans'>
+          className='text-center text-4xl font-bold lg:text-6xl pb-2 pt-8 lg:pb-4 lg:pt-12 font-sans'>
           <span className='text-dark-green'>{language === 'fi-FI' ? 'Keitä' : 'Who'}</span> {language === 'fi-FI' ? 'olemme?' : 'are we?'}
         </h1>
         <p 
-          className='text-center lg:text-xl text-light-gray font-semibold w-3/4 lg:w-1/3 mx-auto pb-10 pt-10'>
+          className='text-center lg:text-xl text-light-gray font-semibold w-3/4 lg:w-[50%] mx-auto pb-10 pt-10'>
           {introduction && introduction}
         </p>
       </div>
 
       {/* Image */}
       <div className='bg-split'>
-        {image2 && <img className='mx-auto w-4/6 h-4/6' src={image2} alt='Football team' />}
+        {image2 && <img className='mx-auto w-4/6 h-4/6 lg:w-[50%] lg:h-[50%] xl:w-[35%] xl:h-[35%]' src={image2} alt='Football team' />}
       </div>
 
       <div className='bg-dark-green'>
@@ -28,10 +28,10 @@ const Identity = ({ introduction, image2, subtitle, activitiesDescription1, acti
           <h2 className='text-center text-white px-[10%] md:px-[20%] text-xl lg:text-4xl font-lexend pt-14 font-semibold'>
             {subtitle && subtitle}
           </h2>
-          <p className='text-center text-white w-3/4 lg:w-1/3 mx-auto pb-10 pt-10 lg:text-xl font-lexend'>
+          <p className='text-center text-white w-3/4 lg:w-[50%] mx-auto pb-10 pt-10 lg:text-xl font-lexend'>
             {activitiesDescription1 && activitiesDescription1}
           </p>
-          <p className='text-center text-white w-3/4 lg:w-1/3 mx-auto pb-14 lg:text-xl font-lexend'>
+          <p className='text-center text-white w-3/4 lg:w-[50%] mx-auto pb-14 lg:text-xl font-lexend'>
             {activitiesDescription2 && activitiesDescription2}
           </p>
         </div>

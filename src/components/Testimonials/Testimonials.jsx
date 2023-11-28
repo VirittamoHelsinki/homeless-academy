48731ed33285ed69830b6807558b2b8443de2688
Henna <57915266+hnnav@users.noreply.github.com>
2023-11-28 14:20:45 +0200

    Centralize items testimonials section

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -24,11 +24,11 @@ const Testimonials = () => {
   }, [language]);
 
   return (
-    <div className='bg-light-green pt-6 pl-9 lg:pl-16 pb-32'>
+    <div className='bg-light-green pt-6 pb-32'>
       <h1 className='font-lexend font-extrabold text-3xl lg:text-5xl text-black text-center pb-10 pt-5'>
         {language === 'en-US' ? 'Testimonials' : 'Kokemuksia'}
       </h1>
-      <div className='grid grid-cols-1 gap-y-28 lg:grid-cols-4 lg:gap-8 w-11/12'>
+      <div className='grid grid-cols-1 gap-y-28 lg:grid-cols-4 lg:gap-8 w-11/12 mx-auto'>
         {testimonials.map((testimonial, index) => (
           <div key={index} className='card h-56 bg-white shadow-2xl relative pb-10 mb-10'>
             <div className='card-body'>

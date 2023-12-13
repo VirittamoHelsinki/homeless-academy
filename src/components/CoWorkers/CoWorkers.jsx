e274b651a8083ddce5934798c4ce474b1118d9d4
Closan Josephine <closjos@digitalentshelsinki.internal>
2023-12-13 08:25:15 +0200

    fix logos alignment in partners section

--- src/components/CoWorkers/CoWorkers.jsx
+++ src/components/CoWorkers/CoWorkers.jsx
@@ -29,9 +29,9 @@ const CoWorkers = () => {
       <h1 className='lg:pb-5 font-lexend text-xl lg:text-3xl font-bold pt-10 pb-6 text-left'>
         {language === 'fi-FI' ? 'Yhteistyökumppanit' : 'Partners'}
       </h1>
-      <div className='lg:flex'>
+      <div className='lg:flex lg:flex-wrap lg:justify-between'>
         {partners.map((partner, index) => (
-          <div key={index}>
+          <div key={index} className='lg:w-1/2'>
             <div>
               <img className='w-56 h-16' src={partner?.logo?.fields?.file?.url} alt='' />
               <p className='py-6 w-5/6'>{partner.description}</p>
@@ -39,10 +39,8 @@ const CoWorkers = () => {
             </div>
           </div>
         ))}
-      </div>
-      <div className='lg:flex  lg:items-start lg:justify-between lg:mr-96 lg:pr-52'>
-        <img className=' w-56 h-16 py-2' src={img3} alt='' />
-        <img className='w-56 h-16 py-2 lg:py-0' src={img4} alt='' />
+        <div className='lg:w-1/2'><img className='w-56 h-16 py-2 lg:py-0' src={img3} alt='' /></div>
+        <div className='lg:w-1/2'><img className='w-56 h-16 py-2 lg:py-0' src={img4} alt='' /></div>
       </div>
     </div>
   );

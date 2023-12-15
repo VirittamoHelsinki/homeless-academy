4c6e0fd410ba6ef006b8890fda330828575a292f
Closan Josephine <closjos@digitalentshelsinki.internal>
2023-12-15 15:17:31 +0200

    fix scroll on top when navigating through pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { scrollToTop } from '../utils/utils';
 
 function Pagination({ articlesPerPage, totalArticles, paginate, currentPage }) {
 
   const totalPages = Math.ceil(totalArticles / articlesPerPage);
   const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
 
-  return (
+  const handlePageClick = (number) => {
+    paginate(number);
+    scrollToTop(); // Call the scrollToTop function here to scrolling to top when navigating through pagination
+  };
+
+   return (
     <div className='join flex items-center justify-center'>
       {pageNumbers.map((number) => (
         <button
           key={number}
-          onClick={() => paginate(number)}
+          onClick={() => handlePageClick(number)}
           className={`join-item btn ${currentPage === number ? 'btn-active' : ''}`}>
           {number}
         </button>

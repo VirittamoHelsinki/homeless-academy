6c39bc9063ae647a7bb2e3ab5073e3ba6380db55
Uehara Tomoko <uehatom@digitalentshelsinki.internal>
2024-02-07 06:46:30 +0200

    edit noPicture error handeling

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -38,22 +38,24 @@ function Contacts() {
   return (
   <div className='bg-medium-green text-center py-10 text-white'>
     <h1 className='font-lexend font-extrabold text-3xl lg:text-5xl p-5'>{language === 'fi-FI' ? 'Homeless Academyn tiimi' : 'Meet our team'}</h1>
-    <div className='flex flex-wrap w-full gap-12 mt-12 px-10'>
+    <div className='flex flex-wrap gap-12 mt-12 px-10 '>
       {contacts.map((contact,index) => (
-        <div key={index} className='flex flex-col gap-2 items-start justify-start max-w-40'>
+        <div key={index} className='flex flex-col gap-2 max-w-40'>
           {/* if user does not put img, it comes empty image  */}
-          <img src={contact.profilePicture?.fields?.file?.url} className='w-36 h-36 rounded-full lg:rounded-sm bg-white'/>
-       {/*    {contact.profilePicture && contact.profilePicture.fields && contact.profilePicture.fields.file.url ? (
+          {/* <img src={contact.profilePicture?.fields?.file?.url} className='w-36 h-36 rounded-full lg:rounded-sm bg-white'/>
+ */}          {contact.profilePicture && contact.profilePicture.fields && contact.profilePicture.fields.file.url ? (
             <img src={contact.profilePicture.fields.file.url} className='w-36 h-36 rounded-full lg:rounded-sm'/>
           ):(
             <div className='font-semibold mt-3'>
               <p>No image</p>
             </div>
-          )} */}
-          <p className='font-semibold mt-3'>{contact.firstName} {contact.lastName}</p>
-          <p>{contact.title}</p>
-          <p>{contact.phoneNumber}</p>
-          <p>{contact.email}</p>
+          )}
+            <div className='text-center'>
+              <p className='font-semibold mt-3'>{contact.firstName} {contact.lastName}</p>
+              <p>{contact.title}</p>
+              <p>{contact.phoneNumber}</p>
+              <p>{contact.email}</p>
+            </div>
         </div>
       ))}
     </div>

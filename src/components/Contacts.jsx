909ffa2eea4b414f4814f10482a65909452a6133
Tomoko-hub <haisaitomoco@gmail.com>
2024-03-05 15:04:38 +0200

    send email, phone issue fixed.

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -36,28 +36,70 @@ function Contacts() {
   }
 
   return (
-  <div className='bg-medium-green text-center py-10 text-white'>
-    <h1 className='font-lexend font-extrabold text-3xl lg:text-5xl p-5'>{language === 'fi-FI' ? 'Homeless Academyn tiimi' : 'Meet our team'}</h1>
-    <div className='flex flex-wrap w-full gap-12 mt-12 px-10'>
-      {contacts.map((contact,index) => (
-        <div key={index} className='flex flex-col gap-2 items-start justify-start max-w-40'>
-          {/* if user does not put img, it comes empty image  */}
-          <img src={contact.profilePicture?.fields?.file?.url} className='w-36 h-36 rounded-full lg:rounded-sm bg-white'/>
-       {/*    {contact.profilePicture && contact.profilePicture.fields && contact.profilePicture.fields.file.url ? (
-            <img src={contact.profilePicture.fields.file.url} className='w-36 h-36 rounded-full lg:rounded-sm'/>
-          ):(
-            <div className='font-semibold mt-3'>
-              <p>No image</p>
+    <>
+    <div className='bg-medium-green py-10 text-white'>
+      <h1 className='font-lexend text-center font-extrabold text-3xl lg:text-5xl p-5'>{language === 'fi-FI' ? 'Homeless Academyn tiimi' : 'Meet our team'}</h1>
+      <div className='flex flex-wrap w-full gap-12 mt-12 px-10 bg-rose-50'>
+        {contacts.map((contact,index) => (
+          <div key={index} className='flex flex-col gap-2 items-start justify-start max-w-40 bg-violet-200'>
+            {/* if user does not put img, it comes empty image  */}
+            <img src={contact.profilePicture?.fields?.file?.url} className='w-36 h-36 rounded-full lg:rounded-sm gray-800'/>
+        {/*    {contact.profilePicture && contact.profilePicture.fields && contact.profilePicture.fields.file.url ? (
+              <img src={contact.profilePicture.fields.file.url} className='w-36 h-36 rounded-full lg:rounded-sm'/>
+            ):(
+              <div className='font-semibold mt-3'>
+                <p>No image</p>
+              </div>
+            )} */}
+            <div className='font-semibold mt-3 text-balance overflow-auto'>
+              <p>{contact.firstName} {contact.lastName}</p>
+              <p>{contact.title}</p>
+              <p>{contact.phoneNumber}</p>
+              <p>{contact.email}</p>
             </div>
-          )} */}
-          <p className='font-semibold mt-3'>{contact.firstName} {contact.lastName}</p>
-          <p>{contact.title}</p>
-          <p>{contact.phoneNumber}</p>
-          <p>{contact.email}</p>
-        </div>
-      ))}
+            <dl className='font-semibold mt-3 '>
+              <dt>{contact.firstName} {contact.lastName}</dt>
+              <dt className='overflow-auto'>{contact.title}</dt>
+              <dt className='underline'><a href={`tel:${contact.phoneNumber}`}>+{contact.phoneNumber}</a></dt>
+              <dt className='underline'><a href={`tel:${contact.phoneNumber}`}>358505780808</a></dt>
+              <dt className='underline'><a href={`mailto:${contact.email}`}>{contact.email}</a></dt>
+            </dl>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
+    <div className='bg-medium-green py-10 text-white'>
+      <h1 className='font-lexend text-center font-extrabold text-3xl lg:text-5xl p-5'>{language === 'fi-FI' ? 'Homeless Academyn tiimi' : 'Meet our team'}</h1>
+      <div className='p-8 mx-auto text-center flex flex-wrap gap-3 bg-rose-50 columns-4'>
+        {contacts.map((contact,index) => (
+          <div key={index} className='flex-initial basis-1/4 bg-violet-200'>
+            {/* if user does not put img, it comes empty image  */}
+            <img src={contact.profilePicture?.fields?.file?.url} className='w-36 h-36 rounded-full lg:rounded-sm gray-800'/>
+        {/*    {contact.profilePicture && contact.profilePicture.fields && contact.profilePicture.fields.file.url ? (
+              <img src={contact.profilePicture.fields.file.url} className='w-36 h-36 rounded-full lg:rounded-sm'/>
+            ):(
+              <div className='font-semibold mt-3'>
+                <p>No image</p>
+              </div>
+            )} */}
+            <div className='font-semibold mt-3 text-balance'>
+              <p>{contact.firstName} {contact.lastName}</p>
+              <p>{contact.title}</p>
+              <p>{contact.phoneNumber}</p>
+              <p>{contact.email}</p>
+            </div>
+            <dl className='font-semibold mt-3 mb-5 overflow-auto'>
+              <dt>{contact.firstName} {contact.lastName}</dt>
+              <dt>{contact.title}</dt>
+              <dt><a href={`tel:${contact.phoneNumber}`}>+{contact.phoneNumber}</a></dt>
+              <dt className='underline'><a href={`tel:${contact.phoneNumber}`}>358505780808</a></dt>
+              <dt className='underline'><a href={`mailto:${contact.email}`}>{contact.email}</a></dt>
+            </dl>
+          </div>
+        ))}
+      </div>
+    </div>
+  </>
   );
 }
 

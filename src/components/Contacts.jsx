eadfaac3d8696f9143fa8b8f6cc7ac0671f88035
Closan Josephine <closjos@digitalentshelsinki.internal>
2024-04-03 15:17:26 +0300

    add layout, hover over the email address

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import AppContext from "../AppContext";
 import { client } from "../client";
+import "./contacts.css";
 
 function Contacts() {
   const { language } = useContext(AppContext);
@@ -36,47 +37,46 @@ function Contacts() {
   }
 
   return (
-    <>
-      <div className="bg-medium-green py-10 text-white">
-        <h1 className="font-lexend text-center font-extrabold text-3xl lg:text-5xl p-5">
-          {language === "fi-FI" ? "Homeless Academyn tiimi" : "Meet our team"}
-        </h1>
-        <div className="p-8 mx-auto text-left flex flex-wrap justify-center gap-12 mt-12 px-10">
-          {contacts.map((contact, index) => (
-            <div
-              key={index}
-              className="flex-initial basis-1/4 max-w-40 lg:max-w-none  flex flex-col items-center justify-center rounded-xl"
-            >
-              {/* Photo Section */}
-              <div className="h-36 w-full flex items-center justify-center mb-4">
-                <img
-                  src={contact.profilePicture?.fields?.file?.url}
-                  className="w-36 h-36 gray-800 relative mx-4 mt-4 rounded-full lg:rounded-sm overflow-hidden"
-                />
-              </div>
+    <div className="bg-medium-green py-10 text-white">
+      <h1 className="font-lexend text-center font-extrabold text-3xl lg:text-5xl p-5">
+        {language === "fi-FI" ? "Homeless Academyn tiimi" : "Meet our team"}
+      </h1>
 
-              {/* Text Section */}
-              <div className="text-center flex-1">
-                <dl className="mt-3 mb-5 overflow-auto">
-                  <dt className="font-semibold">
-                    {contact.firstName} {contact.lastName}
-                  </dt>
-                  <dt>{contact.title}</dt>
-                  <dt className="underline">
-                    <a href={`tel:${contact.phoneNumber}`}>
-                      +{contact.phoneNumber}
-                    </a>
-                  </dt>
-                  <dt className="underline">
-                    <a href={`mailto:${contact.email}`}>{contact.email}</a>
-                  </dt>
-                </dl>
+      <div className="flex flex-wrap justify-center gap-4 sm:gap-6 md:gap-8 lg:gap-10">
+        {contacts.map((contact, index) => (
+          <div
+            key={index}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 p-4"
+          >
+            <div className="block rounded-lg  bg-custom-color shadow-secondary-2 dark:bg-surface-dark h-full flex flex-col pt-8">
+              <img
+                className="w-36 h-36 gray-800 relative mx-auto mt-4 rounded-full  overflow-hidden "
+                src={contact.profilePicture?.fields?.file?.url}
+                alt="profile photo"
+              />
+              <div className="p-6 text-surface dark:text-white text-center flex-grow">
+                <h6 className="mb-2 text-xl font-semibold leading-tight">
+                  {contact.firstName} {contact.lastName}
+                </h6>
+                <p className="mb-4">{contact.title}</p>
+                <p className="mb-4 underline">
+                  <a href={`tel:${contact.phoneNumber}`}>
+                    {" "}
+                    +{contact.phoneNumber}
+                  </a>
+                </p>
+                {/* <p className="mb-4 underline">
+                  <a href={`mailto:${contact.email}`}>{contact.email}</a>
+                </p> */}
+                <p className="mb-4 underline emailLink" data-title={contact.email}>
+                  <a href={`mailto:${contact.email}`} className="px-4 py-2">Email</a>
+                </p>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 

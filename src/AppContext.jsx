5179cb422134db123c8aaeca9a6d63736a9dc916
Tomoko-hub <haisaitomoco@gmail.com>
2024-04-08 14:46:14 +0300

    fix pagenation functionality in navMobile.jsx

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -7,10 +7,9 @@ const AppProvider = ({ children }) => {
 
   const handleShowContactForm = () => document.getElementById('my_modal_1').showModal()
   const handleHideContactForm = () => document.getElementById('my_modal_1').close()
-  const handleHideNavMenu = () => document.getElementById('dropdown-nav').close();
 
   return (
-    <AppContext.Provider value={{ language, setLanguage, handleShowContactForm, handleHideContactForm, handleHideNavMenu }}>
+    <AppContext.Provider value={{ language, setLanguage, handleShowContactForm, handleHideContactForm }}>
       {children}
     </AppContext.Provider>
   );

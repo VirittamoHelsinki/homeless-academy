649ac880a9860a4387cad31b26599b0ac6fe57be
Joni Rinta-Kahila <rintjon@digitalentshelsinki.internal>
2024-04-08 11:00:39 +0300

    fix things

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -7,10 +7,10 @@ const AppProvider = ({ children }) => {
 
   const handleShowContactForm = () => document.getElementById('my_modal_1').showModal()
   const handleHideContactForm = () => document.getElementById('my_modal_1').close()
-  const handleHideNavMenu = () => document.getElementById('dropdown-nav').close();
+  // const handleHideNavMenu = () => document.getElementById('dropdown-nav').close();
 
   return (
-    <AppContext.Provider value={{ language, setLanguage, handleShowContactForm, handleHideContactForm, handleHideNavMenu }}>
+    <AppContext.Provider value={{ language, setLanguage, handleShowContactForm, handleHideContactForm, /*handleHideNavMenu*/ }}>
       {children}
     </AppContext.Provider>
   );
--- src/components/Navbar/NavMobile.jsx
+++ src/components/Navbar/NavMobile.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext, useEffect, useRef } from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { text } from '../../utils/text';
 import AppContext from '../../AppContext';
@@ -8,12 +8,19 @@ import AppContext from '../../AppContext';
 function NavMobile() {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { language, setLanguage, handleShowContactForm, handleHideNavMenu } = useContext(AppContext);
+  const { language, setLanguage, handleShowContactForm, /*handleHideNavMenu*/ } = useContext(AppContext);
   const navBarRef = useRef(null); // Ref to reference the navigation bar button
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
+
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    // setIsMenuOpen(false)
+    setIsMenuOpen(false)
+  }, [pathname])
 
   // Function to close menu when user clicked outside of nav menu
   const handleClickOutside = (event) => {
@@ -21,6 +28,7 @@ function NavMobile() {
       setIsMenuOpen(false);
     }
   };
+
   useEffect(() => {
     document.addEventListener('click', handleClickOutside);
     return () => {
@@ -31,6 +39,10 @@ function NavMobile() {
   const handleChangeLanguage = () => {
     language === 'fi-FI' ? setLanguage('en-US') : setLanguage('fi-FI');
   };
+
+  const handleCurrentLickClick = (to) => {
+    if (pathname === to) setIsMenuOpen(false)
+  }
 
   return (
     <>
@@ -52,49 +64,51 @@ function NavMobile() {
               )}
             </button>
           </div>
-          <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
-            <ul tabIndex={0}
-              className={'menu dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'}
-            >
-              <li>
-                <button
-                  className='px-4 py-2 rounded-3xl bg-blue text-lg font-semibold text-white self-center m-4'
-                  onClick={() => { handleShowContactForm(); setIsMenuOpen(false); }}
-                >{text.contactUs[language]}</button>
-              </li>
-              <li>
-                <button
-                  onClick={() => { handleChangeLanguage(); handleHideNavMenu(); }}
-                >{text.changeLanguage[language]}</button>
-              </li>
-              <li>
-                <Link
-                  to={"/"}
-                  onClick={() => handleHideNavMenu()}
-                >{text.home[language]}</Link>
-              </li>
-              <li>
-                <Link
-                  to={'/about'}
-                  onClick={() => handleHideNavMenu()}
-                >{text.about[language]}</Link>
-              </li>
-              <li>
-                <Link
-                  to={'/news'}
-                  onClick={handleHideNavMenu}
-                >{text.news[language]}</Link>
-              </li>
-              <li>
-                <Link
-                  to={'/events'}
-                  onClick={handleHideNavMenu}
-                >{text.events[language]}</Link>
-              </li>
-            </ul>
-          </nav>
         </div>
       </div>
+
+      <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
+        <ul tabIndex={0}
+          // className={'menu absolute top-0 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'}
+          className="absolute right-0 p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <button
+              className='px-4 py-2 rounded-3xl bg-blue text-lg font-semibold text-white self-center m-4'
+              onClick={() => { handleShowContactForm(); }}
+            >{text.contactUs[language]}</button>
+          </li>
+          <li>
+            <button
+              onClick={() => { handleChangeLanguage(); }}
+            >{text.changeLanguage[language]}</button>
+          </li>
+          <li>
+            <Link
+              to={"/"}
+              onClick={() => handleCurrentLickClick("/")}
+            >{text.home[language]}</Link>
+          </li>
+          <li>
+            <Link
+              to={'/about'}
+              onClick={() => handleCurrentLickClick("/about")}
+            >{text.about[language]}</Link>
+          </li>
+          <li>
+            <Link
+              to={'/news'}
+              onClick={() => handleCurrentLickClick("/news")}
+            >{text.news[language]}</Link>
+          </li>
+          <li>
+            <Link
+              to={'/events'}
+              onClick={() => handleCurrentLickClick("/events")}
+            >{text.events[language]}</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
